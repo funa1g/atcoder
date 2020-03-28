c77852dd11826553f3e0b4d344606e76dea6b606
funa1g <funa1g@gmail.com>
2020-03-29 00:55:17 +0900

    resolve d

--- abc_160/d.py
+++ abc_160/d.py
@@ -3,6 +3,17 @@ n = int(val[0])
 x = int(val[1])
 y = int(val[2])
 
-for i in range(n):
-    print(1)
-    pass
+dists = {}
+for i in range(1, n):
+    dists[i] = 0
+for i in range(1, n+1):
+    for j in range(i+1, n+1):
+        len1 = j - i
+        len2 = abs(j - y) + abs(x - i) + 1
+        len3 = abs(y - i) + abs(j - x) + 1
+        dist = len1 if len1 < len2 else len2
+        dist = dist if dist < len3 else len3
+        dists[dist] += 1
+
+for i in range(1, n):
+    print(dists[i])
